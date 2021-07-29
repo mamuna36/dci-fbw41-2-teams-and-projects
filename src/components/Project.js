71611c92a8c0b67b8db50f56bd5cbb633ddd3027
galymax <werkhowski@gmail.com>
2021-07-29 17:36:22 +0200

    projects

--- src/components/Project.js
+++ src/components/Project.js
@@ -11,7 +11,10 @@ import {
   CardBody,
   CardFooter,
   Badge,
-  Button
+  Button,
+  Nav,
+  NavItem,
+  NavLink
 } from "shards-react";
 
 const Project = ({ project }) => {
@@ -26,19 +29,43 @@ const Project = ({ project }) => {
         </div>
         <CardBody>
           <h5 className="card-title">
-            <a href="#" className="text-fiord-blue">
+            <a href={project.link} className="text-fiord-blue">
               {project.projectTitle}
             </a>
             
           </h5>
+          
           <p className="card-text d-inline-block mb-3">{project.summary}</p>
+          <div className="project-links">
+          <a href={project.link}>
+          <Button pill outline size="sm" className="mb-2">
+        Visit Page
+      </Button>
+      </a>
+      <a href={project.frontendRepo}>
+          <Button pill outline size="sm" className="mb-2">
+        Frontend Repo
+      </Button>
+      </a>
+      <a href={project.backendRepo}>
+          <Button pill outline size="sm" className="mb-2">
+        Backend Repo
+      </Button>
+      </a>
+          
+          </div>
+          <strong className="text-muted d-block mb-2">
+            Team
+          </strong>
           <div className="card-post__author d-flex">
+          
             {project.team.map((member) => {
               return <Person data={member} />
             })}
             
           </div>
-          <span className="text-muted">{/*post.date*/}</span>
+          
+
           <CardFooter className="border-top d-flex">
                 <img src={project.techStack} style={{width: '100%'}} />
             
